41d8cc3cb260e564646f96a9d2934612b03dd443
inno <sayyou0918@gmail.com>
2025-09-09 22:09:40 +0900

    Changing Blog Name

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="bg-background sticky top-0 z-50 border-b">
       <div className="container flex h-[var(--header-height)] items-center px-4">
         <Link href="#" className="text-xl font-semibold">
-          <span className="font-bold">짐코딩 블로그</span>
+          <span className="font-bold">INHO 블로그</span>
         </Link>
         <nav className="ml-auto flex items-center gap-4">
           <Link href="#" className="hover:text-primary font-medium">
