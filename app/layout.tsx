d4b41226a30aff908c0218026ae0b730f294f93d
inno <sayyou0918@gmail.com>
2025-09-09 10:22:19 +0900

    Add about layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import Header from '@/components/layouts/Header';
+import Footer from '@/components/layouts/Footer';
 import './globals.css';
 
 const geistSans = Geist({
@@ -24,7 +26,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <div className="flex min-h-screen flex-col">
+          {/* Header 영역 */}
+          <Header />
+
+          {/* Main 영역 */}
+          <main className="flex-1">{children}</main>
+
+          {/* Footer 영역 */}
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
