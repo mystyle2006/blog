d50f05f1608fab4a38a0aec062d527c9999c4d89
inno <sayyou0918@gmail.com>
2025-09-12 04:05:06 +0900

    Updating Readme

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -5,13 +5,13 @@ export default function Header() {
   return (
     <header className="bg-background sticky top-0 z-50 border-b">
       <div className="container flex h-[var(--header-height)] items-center px-4">
-        <div className="grid w-full grid-cols-3 items-center">
-          <div className="flex items-center justify-start">
+        <div className="grid w-full grid-cols-2 grid-rows-1 md:grid-cols-[200px_1fr_220px] md:items-center md:gap-6">
+          <div className="hidden items-center justify-start md:flex">
             <Link href="/" className="text-xl font-semibold">
               <span className="font-bold">INHO&apos;s Blog</span>
             </Link>
           </div>
-          <nav className="flex items-center justify-center gap-4">
+          <nav className="ml-[9px] flex items-center justify-start gap-4 md:ml-0 md:justify-center">
             <Link href="/" className="hover:text-primary font-medium">
               Home
             </Link>
